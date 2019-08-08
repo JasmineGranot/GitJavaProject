a8a1d4b368712cd6afad0462de493f3e6b3b79b5
Jasmine <yasminso@mta.ac.il>
2019-08-08 23:59:51 +0300

    add vered msg

--- Git project/src/MagitMain.java
+++ Git project/src/MagitMain.java
@@ -1,6 +1,13 @@
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
+
 public class MagitMain {
+    public static final String FILE_NAME = "C:\\Users\\97250\\Downloads\\master.xml";
 
     public static void main(String[] args){
+        fromXmlFileToObject();
         UI uiConsole = new UI();
         try{
             uiConsole.runProgram();
@@ -9,5 +16,23 @@ public class MagitMain {
         catch(Exception e){
             System.out.println("had issues.....");
         }
+    }
+
+    private static void fromXmlFileToObject() {
+        System.out.println("\nFrom File to Object");
+
+        try {
+
+            File file = new File(FILE_NAME);
+            JAXBContext jaxbContext = JAXBContext.newInstance(Magit.class);
+
+            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+//            Customer customer = (Customer) jaxbUnmarshaller.unmarshal(file);
+//            System.out.println(customer);
+
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+
     }
 }
