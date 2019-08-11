a4b4012546b4ba032c13a3df64dc2e80f86cef92
Jasmine <yasminso@mta.ac.il>
2019-08-11 22:09:08 +0300

    added X<L loading

--- Git project/src/MagitUtils.java
+++ Git project/src/MagitUtils.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
