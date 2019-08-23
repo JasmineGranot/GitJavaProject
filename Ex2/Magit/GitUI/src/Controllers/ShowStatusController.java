f0ac1b55e94dbfc2131e7f55f88114d687800eec
Jasmine <yasminso@mta.ac.il>
2019-08-23 10:50:38 +0300

    added show status screen + controller

---
+++ Ex2/Magit/GitUI/src/Controllers/ShowStatusController.java
@@ -0,0 +1,17 @@
+package Controllers;
+
+import javafx.fxml.FXML;
+        import javafx.scene.control.ListView;
+
+public class ShowStatusController {
+
+    @FXML
+    private ListView<?> newFilesListView;
+
+    @FXML
+    private ListView<?> changedFilesListView;
+
+    @FXML
+    private ListView<?> deletedFilesListView;
+
+}
