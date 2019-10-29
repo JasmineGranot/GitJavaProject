8f98bb62e1e891ee18ee81ebced3556c7115417c
Jasmine <yasminso@mta.ac.il>
2019-10-29 06:20:41 +0200

    Merge branch 'testingWebModule' of https://github.com/JasmineGranot/GitJavaProject into testingWebModule
    # Conflicts:
    #	GitEngine/.idea/workspace.xml

--- GitWeb/src/Servlets/SystemServlet.java
+++ GitWeb/src/Servlets/SystemServlet.java
@@ -3,6 +3,7 @@ package Servlets;
 import Exceptions.FileErrorException;
 import GitObjects.UserManager;
 import UIUtils.ServletUtils;
+import Utils.MagitUtils;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -23,15 +24,49 @@ public class SystemServlet extends HttpServlet implements ServletContextListener
 
     @Override
     public void contextDestroyed(ServletContextEvent servletContextEvent) {
-        UserManager userManager = ServletUtils.getUserManager(getServletContext());
-        try {
-            userManager.removeFolders();
+        try{
+            deleteWC(SERVER_PATH, true);
             File serverFolder = new File(SERVER_PATH);
             serverFolder.delete();
         } catch (FileErrorException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
+//        UserManager userManager = ServletUtils.getUserManager(getServletContext());
+//        try {
+//            userManager.removeFolders();
+//            File serverFolder = new File(SERVER_PATH);
+//            serverFolder.delete();
+//        } catch (FileErrorException e) {
+//            e.printStackTrace();
+//        }
 
 
+    }
+
+    private void deleteWC(String filePath, boolean deleteMagit) throws FileErrorException {
+        File root = new File(filePath);
+        String[] files = root.list();
+        String errorMsg;
+
+        if (files != null) {
+            for (String f : files) {
+                File childPath = new File(MagitUtils.joinPaths(filePath, f));
+                if (childPath.isDirectory()) {
+                    if (!childPath.getName().equals(".magit") || deleteMagit) {
+                        deleteWC(childPath.getAbsolutePath(), deleteMagit);
+                        if (!childPath.delete()) {
+                            errorMsg = "Had an issue deleting a file!";
+                            throw new FileErrorException(errorMsg);
+                        }
+                    }
+                }
+                else {
+                    if (!childPath.delete()) {
+                        errorMsg = "Had an issue deleting a file!";
+                        throw new FileErrorException(errorMsg);
+                    }
+                }
+            }
+        }
     }
 }
