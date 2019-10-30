183ceaa4faabc5fb141f4765a78be69fa8f2c79f
Jasmine <yasminso@mta.ac.il>
2019-10-30 15:27:54 +0200

    added wc shit

--- GitWeb/web/Pages/page3/page3_script.js
+++ GitWeb/web/Pages/page3/page3_script.js
@@ -293,42 +293,6 @@ function showCommits(){
 
 }
 
-function showWC(){
-    $.ajax({
-        url: REPO_ACTIONS,
-        data:
-            {
-                action: "showWC",
-            },
-        success: function (res) {
-            if(res.harError) {
-                alert(res.errorMsg);
-            }
-            else {
-                showWCStatus(res.res);
-            }
-        }
-    });
-}
-
-function showWCStatus(files) {
-    var filesArea = $('#workingCopy');
-
-    filesArea.empty();
-
-    $.each(files || [], function (index, file) {
-        var newFile = $(document.createElement('ul', { is : 'expanding-list' }));
-
-
-        newFile.text(file);
-        newFile.appendTo(filesArea);
-
-        $(newFile).click(function () {
-            alert(file);
-        });
-    });
-}
-
 function createPullRequest(){
     var srcBranch=prompt("Please enter the source branch:");
     var targetBranch=prompt("Please enter the target branch:");
@@ -440,9 +404,6 @@ function updatePullRequestsForUser(pullRequests) {
         });
     });
 }
-
-
-
 
 // ========================= On loading ================================
 function triggerGetMsg(){
